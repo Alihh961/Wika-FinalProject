b529738b6e5f1f56176852201ce9f94a30db2439
Alihh961 <hajhassan.ali@outlook.com>
2023-05-13 03:20:28 +0200

    addresss input update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, ElementRef, Input, InputDecorator, ViewChild } from '@angular/core';
 import { Feature, FeatureCollection } from '../Interface/AddressResults';
 import { User } from '../Interface/userdetails';
-import { NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 
 
 
@@ -29,10 +29,7 @@ export class LoginComponent {
     gender: ''
   };
 
-  maxDay!: number ;
-  maxMonth!: number;
-  maxYear!: number;
-  maxDate!: string;
+  maxDate!: string; // maxDate for the calendar to prevent under 18 from inscrire
 
 
   // * variables related to the component class file 
@@ -44,8 +41,10 @@ export class LoginComponent {
   @ViewChild("signupspan") signupspan !: ElementRef;
   @ViewChild("suggestions") suggestions !: ElementRef;
   @ViewChild("addressResults") addressResults !: ElementRef;
-  @ViewChild("input") input !: ElementRef;
-  // @ViewChild("registrationForm") regForm !: FormGroup;
+  @ViewChild("input") input !: ElementRef; // input address
+  @ViewChild("submitButton") submitbutton !: ElementRef;
+
+  @ViewChild("registrationForm") regForm !: FormGroup;
 
 
 
@@ -60,8 +59,13 @@ export class LoginComponent {
     this.autorizedAgeOfNewUsers();
   }
 
+  //* searching for address when a change happens
+  searchingAddress(value: string): object {
 
-  searchingAddress(value: string) {
+    if (!value) {
+      //* idsplay none for the previous results if the input value becomes null
+      this.addressResults.nativeElement.style.display = "none";
+    }
 
     const url = `https://api-adresse.data.gouv.fr/search/?q=${value}&limit=10`;
 
@@ -75,7 +79,8 @@ export class LoginComponent {
 
   }
 
-  selectaddress(divElement: MouseEvent): void { // selecting the address on click event
+  // selecting the address on click event
+  selectaddress(divElement: MouseEvent): void {
 
     // targeting the click Div
     const address = divElement.target as HTMLDivElement;
@@ -93,7 +98,6 @@ export class LoginComponent {
     this.suggestions.nativeElement.innerHTML = "";
   }
 
-
   // Reset the input to set a new address
   resetInput(): void {
     // undisabled the input to set a new address
@@ -103,11 +107,7 @@ export class LoginComponent {
     this.input.nativeElement.value = "";
   }
 
-
-
-
   //* changing the color of span signin and signup on click in the arrow
-
   changingColorOfSpan(): void {
     if (getComputedStyle(this.signinspan.nativeElement).color == "rgb(8, 129, 120)") {
       this.signinspan.nativeElement.style.color = "rgb(196, 195, 202)";
@@ -128,25 +128,27 @@ export class LoginComponent {
 
   }
 
-  autorizedAgeOfNewUsers() {
+  //* displaying only the valid date of people over than 18 years old
+  autorizedAgeOfNewUsers(): void {
     const currentDate: Date = new Date();
 
     const currentYear: number = currentDate.getFullYear();
     const currentMonth: number = currentDate.getMonth();
     const currentDay: number = currentDate.getDate();
 
-    this.maxYear = currentYear - 18;
-    this.maxMonth = currentMonth;
-    this.maxDay = currentDay;
-
-    const max = new Date(this.maxYear , this.maxMonth , this.maxDay);
+    const max = new Date(currentYear - 18, currentMonth, currentDay);
     this.maxDate = max.toISOString().slice(0, 10); // get "YYYY-MM-DD" format
 
-    
+
   }
 
+  //* checking the value of address on blur
+  onBlur(element: any) {
+    const input = element.target as HTMLInputElement;
+    if (!input.hasAttribute("disabled") && this.regForm.invalid)
+      this.submitbutton.nativeElement.setAttribute("disabled", "");
 
-
+  }
 }
 
 
